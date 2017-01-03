a8a910a50dd26361efb4921ea4dc2c8d4fd5e117
tranhoanghiep <hiep.tranhoang@homedirect.com.vn>
2017-01-03 15:46:19 +0700

    ng2BCCopy continue

---
+++ ng2BCCopy/app/products/product.ts
@@ -0,0 +1,13 @@
+//Data Transfer onject (DTO)
+//without any logic
+export interface IProduct {
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    price: string;
+    description: string;
+    starRating: string;
+    catId: string;
+    imageUrl: string;
+}
